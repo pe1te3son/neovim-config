f0d20a8805726b3f24aa7816e13cab63d1fe16fc
Peter Janak <peter.janak@ims-evolve.com>
2024-02-26 11:06:38 +0000

    clean up colors

--- lua/workconfig/set.lua
+++ lua/workconfig/set.lua
@@ -8,7 +8,7 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
--- vim.opt.smartindent = true
+vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
@@ -20,7 +20,7 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+vim.opt.termguicolors = false
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
