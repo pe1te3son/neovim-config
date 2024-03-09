050f1df7fdfec315eb8a1dc37cc2cf91e360db55
Peter Janak <2010janak2010@gmail.com>
2024-03-09 21:33:55 +0000

    add map for copilot chat about selected code

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -11,6 +11,14 @@ vim.keymap.set("n", "<leader>cp", function()
   end,
   { noremap = true })
 
+vim.keymap.set("v", "<leader>cp", function()
+    local input = vim.fn.input("Chat about selection: ")
+    if input ~= "" then
+      co_chat.ask(input, { selection = require("CopilotChat.select").selection })
+    end
+  end,
+  { noremap = true })
+
 
 vim.keymap.set("v", "<leader>ce", function()
     co_chat.ask("Explain how it works", { selection = require("CopilotChat.select").selection })
