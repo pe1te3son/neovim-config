abb2b9665dfa6e1ac0308e2f2b9284fab4d3ff3f
Peter Janak <2010janak2010@gmail.com>
2024-03-13 19:10:39 +0000

    improve buffer switching when split window

--- after/plugin/buffer.lua
+++ after/plugin/buffer.lua
@@ -1,5 +1,5 @@
 -- spacemacs like buffer management
-local function switch_to_last_buffer()
+local function find_last_buffer_listed()
   local all_buffers = vim.api.nvim_list_bufs()
   local current_buffer = vim.api.nvim_get_current_buf()
   local local_buffers = {}
@@ -13,8 +13,20 @@ local function switch_to_last_buffer()
   end
 
   if #local_buffers > 0 then
-    local last_buffer = local_buffers[#local_buffers]
-    vim.api.nvim_set_current_buf(last_buffer)
+    return local_buffers[#local_buffers]
+  end
+end
+
+local function switch_to_last_buffer()
+  local prev_bufnr = vim.api.nvim_eval('bufnr("#")')
+
+  if vim.api.nvim_buf_get_option(prev_bufnr, 'buflisted') then
+    return vim.api.nvim_set_current_buf(prev_bufnr)
+  end
+
+  local last_on_list = find_last_buffer_listed()
+  if last_on_list then
+    vim.api.nvim_set_current_buf(last_on_list)
   end
 end
 
@@ -22,8 +34,8 @@ local function delete_buffer_by_id(buffer_id)
   vim.api.nvim_command('bdelete ' .. buffer_id)
 end
 
-local function is_modified(buffr)
-  return vim.api.nvim_buf_get_option(buffr, 'modified')
+local function is_modified(bufnr)
+  return vim.api.nvim_buf_get_option(bufnr, 'modified')
 end
 
 vim.keymap.set("n", "<leader>bd", function()
