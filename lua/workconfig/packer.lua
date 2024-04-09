493f62a9f7712faa98702c2a106ccaeffde6732f
Peter Janak <peter.janak@ims-evolve.com>
2024-04-09 12:18:25 +0100

    update telescope mapping for buffers

--- lua/workconfig/packer.lua
+++ lua/workconfig/packer.lua
@@ -7,7 +7,7 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    'nvim-telescope/telescope.nvim', tag = '0.1.x',
     requires = { { 'nvim-lua/plenary.nvim' } }
   }
   use { 'nvim-telescope/telescope-ui-select.nvim' }
